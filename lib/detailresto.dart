ef8dc287dfc2be3e564a1b3fee767a2526995f1a
elroiyoshua <elroiyoshua@student.telkomuniversity.ac.id>
2023-08-30 16:06:41 +0700

    update layout

--- lib/detailresto.dart
+++ lib/detailresto.dart
@@ -2,67 +2,106 @@ import 'package:flutter/material.dart';
 import 'package:flutter_restaurant/resources.dart';
 
 class detailResto extends StatelessWidget {
-  static const routeName ='/detailresto';
+  static const routeName = '/detailresto';
   final Restaurant restaurant;
 
-  const detailResto({Key? key,required this.restaurant}) : super(key: key);
+  const detailResto({Key? key, required this.restaurant}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: NestedScrollView(headerSliverBuilder: (context,isScrolled){
-        return[
-          SliverAppBar(expandedHeight: 300,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.network(restaurant.pictureId,
-                fit: BoxFit.fitHeight ,)
-              ,
+    return SafeArea(
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (context, isScrolled) {
+            return [
+              SliverAppBar(
+                expandedHeight: 300,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.network(
+                    restaurant.pictureId,
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              )
+            ];
+          },
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    restaurant.name,
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Located at ${restaurant.city} with rate ${restaurant.rating}",
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    restaurant.description,
+                    style: TextStyle(fontSize: 12, color: Colors.black54),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Foods:",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: restaurant.foods.items.map((food) {
+                      return ListTile(
+                        leading: Hero(
+                          tag: Object(),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Image.asset('assets/food.png'),
+                          ),
+                        ),
+                        title: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(food.name),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Drinks:",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: restaurant.drinks.items.map((drink) {
+                      return ListTile(
+                        leading: Hero(
+                          tag: Object(),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Image.asset('assets/soda.png'),
+                          ),
+                        ),
+                        title: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(drink.name),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
             ),
-            )
-        ];
-      },body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(restaurant.name,style: TextStyle(fontSize: 40),),
-              SizedBox(height: 5,),
-              Text("Located at ${restaurant.city} with rate ${restaurant.rating}",style: Theme.of(context).textTheme.labelLarge,),
-              SizedBox(height: 5,),
-              Text(restaurant.description,style: TextStyle(fontSize: 12,color: Colors.black54),),
-              SizedBox(height: 10),
-              Text(
-                "Foods:",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: restaurant.foods.items.map((food) {
-                  return ListTile(
-                    title: Text(food.name),
-                  );
-                }).toList(),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Drinks:",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: restaurant.drinks.items.map((drink) {
-                  return ListTile(
-                    title: Text(drink.name),
-                  );
-                }).toList(),
-              ),
-            ],
           ),
-        ) ,),
+        ),
       ),
-    ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,43 +18,49 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: MyHomePage.routeName,
       routes: {
-        MyHomePage.routeName : (context) => const MyHomePage(),
-        detailResto.routeName : (context) => detailResto(restaurant: ModalRoute.of(context)?.settings.arguments as Restaurant),
+        MyHomePage.routeName: (context) => const MyHomePage(),
+        detailResto.routeName: (context) => detailResto(
+            restaurant:
+                ModalRoute.of(context)?.settings.arguments as Restaurant),
       },
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  static const routeName ='/home_page';
+  static const routeName = '/home_page';
+
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      appBar: AppBar(title: Text("Resto App"),
-      elevation: 0,
-      ),
-      body: FutureBuilder(
-        future: DefaultAssetBundle.of(context).loadString('assets/local_restaurant.json'),
-        builder: (context,snapshot){
-          final List<Restaurant> restaurant = parseResto(snapshot.data);
-          return ListView.builder(
-              itemCount: restaurant.length,
-              itemBuilder: (context,index){
-                return CardResto(context,restaurant[index]);
-              }
-          );
-        },)
-      ),
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Resto App"),
+            elevation: 0,
+          ),
+          body: FutureBuilder(
+            future: DefaultAssetBundle.of(context)
+                .loadString('assets/local_restaurant.json'),
+            builder: (context, snapshot) {
+              final List<Restaurant> restaurant = parseResto(snapshot.data);
+              return ListView.builder(
+                  itemCount: restaurant.length,
+                  itemBuilder: (context, index) {
+                    return CardResto(context, restaurant[index]);
+                  });
+            },
+          )),
     );
   }
 }
 
-Widget CardResto(BuildContext context,Restaurant restaurant){
+Widget CardResto(BuildContext context, Restaurant restaurant) {
   return GestureDetector(
-    onTap: (){
-      Navigator.pushNamed(context, detailResto.routeName,arguments: restaurant);
+    onTap: () {
+      Navigator.pushNamed(context, detailResto.routeName,
+          arguments: restaurant);
     },
     child: Card(
       margin: EdgeInsets.all(8.0),
@@ -61,40 +68,68 @@ Widget CardResto(BuildContext context,Restaurant restaurant){
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(padding: EdgeInsets.only(left: 10),
-              child: ClipRRect(borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                child: Image.network(restaurant.pictureId,height: 150,width: 150,)) ,
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  child: Image.network(
+                    restaurant.pictureId,
+                    height: 150,
+                    width: 150,
+                  )),
             ),
-
-            Padding(padding: EdgeInsets.only(left: 20),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
               child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(height: 30,),
-                  Row(
-                    children : [
-                      Icon(Icons.restaurant,size: 20,),Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: Text(restaurant.name,style: TextStyle(fontSize: 13,
-                            fontWeight: FontWeight.bold),),
-                      ),],
+                  SizedBox(
+                    height: 30,
                   ),
-                  SizedBox(height: 10,),
-                  Row(children: [
-                    Icon(Icons.location_city),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(restaurant.city),
-                    )
-                  ],),
-                  SizedBox(height: 10,),
-                  Row(children: [
-                    Icon(Icons.star,color: Colors.yellow,),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(restaurant.rating.toString()),
-                    )
-                  ],),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.restaurant,
+                        size: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          restaurant.name,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.location_city),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(restaurant.city),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(restaurant.rating.toString()),
+                      )
+                    ],
+                  ),
                 ],
               ),
             )
@@ -104,6 +139,3 @@ Widget CardResto(BuildContext context,Restaurant restaurant){
     ),
   );
 }
-
-
-
